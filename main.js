aa12b8ed40ecd0bfdbf8f199049820e0e0f8f3fc
SSaajan <50995007+SSaajan@users.noreply.github.com>
2022-12-19 21:22:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const htmlClasses = [
 
 const submitForm = document.getElementById("submitForm");
 const words = ["/////", "> Initializing services", "> [success]", "> Commencing communication with services", ">> ## DSP | ADAP | CASB ##", ">> Instance ID #2112", "welcome..."];
-const typeSpeed = [0, 50, 200, 600];
+const typeSpeed = [0, 50, 200, 400];
 let loaderCounter = 0, wordIndex = 0, charIndex = 0, htmlIndex = 0, speedIndex = 2;
 
 function loadType() {
@@ -29,7 +29,7 @@ function eraseLoader() {
     htmlClasses[htmlIndex].textContent = "";
     loaderCounter++;
     charIndex = 0;
-    if(loaderCounter < 4) {
+    if(loaderCounter < 3) {
         loadType();
     }  
     else {
@@ -52,6 +52,8 @@ function type() {
     if(wordIndex == words.length) {
         document.querySelector(".cursor").textContent = "";
         document.querySelector(".typedWelcome").textContent = "";
+        document.getElementById("tree").style.visibility = "visible";
+        document.getElementById("snowflake").style.visibility = "visible";
         document.getElementById("teamIDForm").style.visibility = "visible";
     }
     else {
@@ -76,30 +78,111 @@ function nextWord() {
         setTimeout(type, 2000);
     }
     else {
-        setTimeout(type, 1000);
+        setTimeout(type, 500);
     }
+}
+
+function hideTeamID() {
+    document.getElementById("teamIDLabel").style.visibility = "hidden";
+    document.getElementById("teamID").style.visibility = "hidden";
+    document.getElementById("teamIDSubmit").style.visibility = "hidden";
+    document.getElementById("passphraseLabel").style.visibility = "visible";
+    document.getElementById("passphrase").style.visibility = "visible";
+    document.getElementById("key").style.visibility = "visible";
+    document.getElementById("keyLabel").style.visibility = "visible";
+    document.getElementById("passphraseSubmit").style.visibility = "visible";
 }
 
 function sendData() {
     const XHR = new XMLHttpRequest();
-    const data = new FormData(submitForm);
+    
+    let teamID = document.getElementById("teamID").value;
+    let passphrase = document.getElementById("passphrase").value;
+    let key = document.getElementById("key").value;
+    let data = new Object();
+    data.teamID = teamID;
+    data.passphrase = passphrase;
+    data.key = key;
+    data = JSON.stringify(data);
 
     XHR.addEventListener("load", (event) => {
-        alert(event.target.responseText);
-      });
+        document.getElementById("passphraseLabel").style.visibility = "hidden";
+        document.getElementById("passphrase").style.visibility = "hidden";
+        document.getElementById("key").style.visibility = "hidden";
+        document.getElementById("keyLabel").style.visibility = "hidden";
+        document.getElementById("passphraseSubmit").style.visibility = "hidden";
+        document.getElementById("success").style.visibility = "visible";
+
+        let result = JSON.parse(event.target.responseText);
+
+        if(result.Status) {
+            if(result.Status == "Failed") {
+                document.getElementById("mainMsg").textContent = "Decryption failed";  
+                document.getElementById("mainMsg").style.color = "#ff2626";
+                document.getElementById("subText").textContent = "Your encryption text/decryption key is invalid.";
+                document.getElementById("subText2").style.visibility = "hidden";
+                document.getElementById("subTextWon").style.visibility = "hidden";
+                alertColour();
+                
+                document.getElementById("retry").style.visibility = "visible";
+            }
+            else {
+                document.getElementById("mainMsg").textContent = "ACCESS DENIED";
+                document.getElementById("mainMsg").style.color = "#ff2626";
+                document.getElementById("subText").textContent = "Only one submission per team is allowed.";
+                document.getElementById("subText2").style.visibility = "hidden";
+                document.getElementById("subTextWon").style.visibility = "hidden";
+                alertColour();
+            }
+        }
+        else {
+            if(result.place && result.place == 1) {
+                document.getElementById("subTextWon").textContent = "FIRST PLACE!";
+            }
+            else if(result.place && result.place == 2) {
+                document.getElementById("subTextWon").textContent = "SECOND PLACE!";
+            }
+            else if(result.place && result.place == 3) {
+                document.getElementById("subTextWon").textContent = "THIRD PLACE!";
+            }
+            else if(result.place && result.place > 3) {
+                document.getElementById("mainMsg").textContent = "Decryption Successful, but you're too late."; 
+                document.getElementById("subText").textContent = "3 teams have already claimed the prize :(";
+                document.getElementById("subText2").style.visibility = "hidden";
+                document.getElementById("subTextWon").style.visibility = "hidden";
+            }   
+        }
+    });
   
-      // Define what happens in case of error
-      XHR.addEventListener("error", (event) => {
-        alert('Oops! Something went wrong.');
-      });
+    // Define what happens in case of error
+    XHR.addEventListener("error", (event) => {
+        alertColour();
+        alert("API Error. Please contact event incharges - Saajan / Devnath / Dhanabal / Rajkumar");
+    });
 
     // Set up our request
-    XHR.open("GET", "https://stellular-bombolone-ba80b2.netlify.app/.netlify/functions/api");
+    XHR.open("POST", "https://stellular-bombolone-ba80b2.netlify.app/.netlify/functions/api");
+    XHR.setRequestHeader("Accept", "application/json");
+    XHR.setRequestHeader("Content-Type", "application/json");
 
     // The data sent is what the user provided in the form
     XHR.send(data);
 }
 
+function alertColour() {
+    document.getElementById("treeInner").style.color = "#ff2626";
+    document.getElementById("star").style.color = "#ff2626";
+    document.getElementById("snowflake").style.color = "#ff2626";
+    document.getElementById("treeInner").style.color = "#ff2626";
+    document.getElementById("tree").style.color = "#ff2626";
+    document.getElementById("snowflake").style.color = "#ff2626";
+    document.getElementById("initialTexts").style.color = "#ff2626";
+    document.getElementById("container").style.color = "#ff2626";
+    document.getElementById("footerText").style.color = "#ff2626";
+    document.getElementById("star").style.animation = "none";
+    document.getElementById("tree").style.animation = "none";
+    document.getElementById("snowflake").style.animation = "none";
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     loadType();
